632a2e5b41e60135a0a9974ca092206c9b2695e5
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2017-11-20 16:24:39 +0000

    Add files via upload

---
+++ scping_from_archer_to_cx1.sh
@@ -0,0 +1,70 @@
+#
+
+DIRS_ARCHER="
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/PBE-D3/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08/DISPERSI/125.845303/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/PBE-D3/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98/DISPERSI/116.974380/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/PBE/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/128.958411/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/PBE/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98_10/DISPERSI/116.724991/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/B3LYP-D3/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/122.876461/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/B3LYP-D3/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98_10/DISPERSI/117.295560/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/PBE0/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/125.870214/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/PBE0/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98_10/DISPERSI/118.579997/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/PBE0-DC/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/123.614316/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/PBE0-DC/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98_10/DISPERSI/116.445451/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/LDA/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/124.245016/SCELPHONO_444
+/work/e05/e05/dcarrras/pob_TZVP/Calcite_I/LDA/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/121.758321/SCELPHONO_444
+"
+
+DIRS_cx1="
+/work/dcarrasc/pob_TZVP/Calcite_I/PBE-D3/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08/DISPERSI/125.845303/
+/work/dcarrasc/pob_TZVP/Calcite_I/PBE-D3/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98/DISPERSI/116.974380/
+/work/dcarrasc/pob_TZVP/Calcite_I/PBE/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/128.958411/
+/work/dcarrasc/pob_TZVP/Calcite_I/PBE/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98_10/DISPERSI/116.724991/
+/work/dcarrasc/pob_TZVP/Calcite_I/B3LYP-D3/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/122.876461/
+/work/dcarrasc/pob_TZVP/Calcite_I/B3LYP-D3/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98_10/DISPERSI/117.295560/
+/work/dcarrasc/pob_TZVP/Calcite_I/PBE0/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/125.870214/
+/work/dcarrasc/pob_TZVP/Calcite_I/PBE0/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98_10/DISPERSI/118.579997/
+/work/dcarrasc/pob_TZVP/Calcite_I/PBE0-DC/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/123.614316/
+/work/dcarrasc/pob_TZVP/Calcite_I/PBE0-DC/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.87_0.98_10/DISPERSI/116.445451/
+/work/dcarrasc/pob_TZVP/Calcite_I/LDA/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/124.245016/
+/work/dcarrasc/pob_TZVP/Calcite_I/LDA/SHRINK_8_8__bipolar_18_18__TOLINTEG_8_18__XXLGRID_TOLDEE_8/EOS/0.98_1.08_10/DISPERSI/121.758321/
+"
+
+archer_ssh="dcarrras@login.archer.ac.uk"
+cx1_ssh="dcarrasc@login.cx1.hpc.ic.ac.uk"
+
+echo $archer_ssh
+
+
+# Put them into BASH array:
+archer=(${DIRS_ARCHER})
+cx1=(${DIRS_cx1})
+
+echo ${archer[*]}
+
+echo "archer[0] is ${archer[0]}"
+echo cx1
+
+# get the length of the arrays
+#length=${#archer[@]}
+
+# get the indexes of the arrays
+indexes=${!archer[*]}
+
+echo "Indexes are $indexes " # to check
+
+for index in $indexes; do
+# echo "${archer[$index]} is in ${cx1[$index]}"
+  # If you want to add a password to avoid introducing it 10 times:
+# spawn scp -rp ${archer_ssh}:${archer[$index]} ${cx1[$index]}
+  scp -rp ${archer_ssh}:${archer[$index]} ${cx1[$index]}
+# expect "Password: "
+# send "\r"
+# expect "*\r"
+# expect "\r"
+# expect eof
+# expect "*\r"
+
+done
+
+
